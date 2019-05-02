85ed65ee25cb224bcd3eaab64cd718fbaea1c6d4
mohamedsubhi <mohamedsubhibouchi@gmail.com>
2019-05-02 15:19:03 +0200

    store different type in an array and override tostring method and using arraylist class

--- CsharpCourse/Program.cs
+++ CsharpCourse/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections;
 
 namespace CsharpCourse
 {
@@ -10,6 +7,37 @@ namespace CsharpCourse
     {
         static void Main(string[] args)
         {
+            //object[] array = new object[3];
+            //array[0] = 1;
+            //array[1] = "item2";
+
+            Customer c = new Customer();
+            c.ID = 99;
+            c.Name = "customer1";
+
+            //array[2] = c;
+
+            ArrayList array = new ArrayList();
+            array.Add(1);
+            array.Add("item2");
+            array.Add(c);
+
+            foreach (object obj in array)
+            {
+                Console.WriteLine(obj);
+            }
+            Console.ReadKey();
+        }
+    }
+
+    class Customer
+    {
+        public string Name { get; set; }
+        public int ID { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
         }
     }
 }
