8c1a7c58b15eb2a2708b6921e0282dad5eed258c
mohamedsubhi <mohamedsubhibouchi@gmail.com>
2019-05-05 10:09:19 +0200

    recursive functions

---
+++ Recursive.cs/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Recursive.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a number");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            double factorial = Factorial(number);
+
+            Console.WriteLine(factorial.ToString());
+        }
+
+        public static double Factorial(int number)
+        {
+            if (number == 0)
+            {
+                return 1;
+            }
+
+            //double factorial = 1;
+
+            //for (int i = number; i >= number; i++)
+            //{
+            //    factorial = factorial * i;
+            //}
+
+            //return factorial;
+
+            return number * Factorial(number - 1);
+        }
+    }
+}
