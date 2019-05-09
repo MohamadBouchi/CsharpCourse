e4e6ae8652d8284e719681bf3c83a3a96ab8b9dc
mohamedsubhi <mohamedsubhibouchi@gmail.com>
2019-05-09 16:22:23 +0200

    finally block execeptions

---
+++ FinallyBlock/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinallyBlock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Hello();
+                Console.ReadKey();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+            }
+        }
+
+        public static void Hello()
+        {
+            try
+            {
+                throw new Exception("Try Exception");
+            }
+            //catch
+            //{
+
+            //}
+            finally
+            {
+                throw new Exception("Finally Exception");
+                //Console.WriteLine("this line will be executed");
+                //int result = Convert.ToInt32("TEN");
+                //Console.WriteLine("this line will not be executed");
+            }
+        }
+    }
+}
