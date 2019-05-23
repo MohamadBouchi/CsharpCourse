971e213af5f600a62a4b8cbf65f1f7fcab2cc7d7
mohamedsubhi <mohamedsubhibouchi@gmail.com>
2019-05-23 10:09:23 +0200

    type casting example

--- DiffBetweweenIsAndAs/Program.cs
+++ DiffBetweweenIsAndAs/Program.cs
@@ -12,10 +12,22 @@ namespace DiffBetweweenIsAndAs
             //    name = "Mark"
             //};
 
-            Employee emp = new PermenetEmployee
+            //Employee emp = new PermenetEmployee
+            //{
+            //    ID = 101,
+            //    name = "Mark"
+            //};
+
+            Employee emp1 = new PermenetEmployee
             {
                 ID = 101,
                 name = "Mark"
+            };
+
+            Employee emp2 = new ContractEmployee
+            {
+                ID = 102,
+                name = "Mary"
             };
 
             //Employee emp = new PermenetEmployee
@@ -63,19 +75,34 @@ namespace DiffBetweweenIsAndAs
             //    Console.ReadKey();
             //}
 
-            PermenetEmployee permenetEmployee = emp as PermenetEmployee;
+            //PermenetEmployee permenetEmployee = emp as PermenetEmployee;
 
-            if (permenetEmployee == null)
-                Console.WriteLine("permenetEmployee is null");
+            //if (permenetEmployee == null)
+            //    Console.WriteLine("permenetEmployee is null");
+            //else
+            //    Console.WriteLine("permenetEmployee is not null");
+
+            //ContractEmployee contractEmployee = emp as ContractEmployee;
+
+            //if (contractEmployee == null)
+            //    Console.WriteLine("contractEmployee is null");
+            //else
+            //    Console.WriteLine("contractEmployee is not null");
+
+            Employee employee1 = emp1 as Employee;
+            if (employee1 == null)
+                Console.WriteLine("employee1 is null");
             else
-                Console.WriteLine("permenetEmployee is not null");
+                Console.WriteLine("employee1 is not null");
 
-            ContractEmployee contractEmployee = emp as ContractEmployee;
-
-            if (contractEmployee == null)
-                Console.WriteLine("contractEmployee is null");
+            Employee employee2 = emp2 as Employee;
+            if (employee2 == null)
+                Console.WriteLine("employee2 is null");
             else
-                Console.WriteLine("contractEmployee is not null");
+                Console.WriteLine("employee2 is not null");
+
+            Employee emp3 = new Employee() { ID = 103, name = "Moody" };
+            PermenetEmployee pEmp = (PermenetEmployee)emp3;
         }
 
         class Employee
